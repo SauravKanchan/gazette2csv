e9bd89b10298cda9d0dfc9c5beb68dfea07cacb5
SauravKanchan <sauravnk30@gmail.com>
2018-07-30 02:05:37 +0530

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # gazette2csv
+```
+python main.py <pdf_filename>
+
+Eg:- python main.py comps_sem_8
+```
+
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os,sys,csv,re
 os.system("java -jar tabula-1.0.2-jar-with-dependencies.jar -p all -o {0}.csv {0}.pdf".format(sys.argv[1]))
 res_a=""
-
 def get_matrix(row1, row2 ,row3):
     row_list1 = [];row_list2 = [];row_list3 = []
     for i in range(0,len(row3)-1,2):
@@ -18,17 +17,17 @@ with open(sys.argv[1]+'.csv') as csvfile:
     sub = re.split("\d\.",reader[14][0])[1:]+re.split("\d\.",reader[15][0])[1:]
     no2sub = reader[8][0].count("--->")
     for row in range(17,len(reader),62):
-        for sp_row in range(0,28,9):
-            if '-------' not in reader[row+sp_row][0]:
-                name_ls=reader[row+sp_row][0].split()
+        for sr in range(0,28,9):
+            if '-------' not in reader[row+sr][0]:
+                name_ls=reader[row+sr][0].split()
                 res_a+=name_ls[0]+", "+ " ".join(name_ls)[len(name_ls[0]):-36] +", "
-                a,b,c=get_matrix(reader[row+sp_row+2][0]+reader[row+sp_row+2][1][:2], reader[row+sp_row+3][0]+reader[row+sp_row+3][1][:2] ,reader[row+sp_row+4][0]+reader[row+sp_row+4][1][:2])
+                a,b,c=get_matrix(reader[row+sr+2][0]+reader[row+sr+2][1][:2],reader[row+sr+3][0]+reader[row+sr+3][1][:2],reader[row+sr+4][0]+reader[row+sr+4][1][:2])
                 for i in range(len(a)):
                     res_a+=str(a[i])+", "+str(b[i])+","+str(c[i])+", "
                 res_a+="\n"
-    a=[reader[9][0][i:i+9].replace("---","-"*9) for i in range(0, len(reader[9][0]), 9)]   + [reader[9][1][:9]]
-    b=[reader[10][0][i:i+9].replace("---","-"*9) for i in range(0, len(reader[10][0]), 9)]  + [reader[10][1][:9]]
-    c=[reader[11][0][i:i+3].replace("---","-"*3) for i in range(0, len(reader[11][0]), 3)] + [reader[11][1][:3]  ]
+    a=[reader[9][0][i:i+9].replace("---","-"*9) for i in range(0, len(reader[9][0]), 9)]  + [reader[9][1][:9]]
+    b=[reader[10][0][i:i+9].replace("---","-"*9) for i in range(0, len(reader[10][0]), 9)]+ [reader[10][1][:9]]
+    c=[reader[11][0][i:i+3].replace("---","-"*3) for i in range(0, len(reader[11][0]), 3)]+ [reader[11][1][:3]]
 
 with open(sys.argv[1]+".csv","w") as csvfile:
     sub_str = " , , "
@@ -39,5 +38,4 @@ with open(sys.argv[1]+".csv","w") as csvfile:
     res_b=" , , "
     for i in range(len(a)):
         res_b+=str(a[i])+", "+str(b[i])+","+str(c[i])+", "
-    csvfile.write(res_b+"\n")
-    csvfile.write(res_a)
+    csvfile.write(res_b+"\n"+res_a)
